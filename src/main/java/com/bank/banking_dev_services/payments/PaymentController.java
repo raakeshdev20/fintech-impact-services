30562c877f40cc59bde7a3172ecf73a2cf893694
Raakesh Rajagopalan <raakesh20@gmail.com>
2026-04-27 20:44:19 -0400

    Modified file to check html reports

--- src/main/java/com/bank/banking_dev_services/payments/PaymentController.java
+++ src/main/java/com/bank/banking_dev_services/payments/PaymentController.java
@@ -23,7 +23,7 @@ public class PaymentController {
         PaymentResponse response = new PaymentResponse(
                 success ? "SUCCESS" : "FAILED",
                 success ? "Payment Successful" : "Payment Failed",
-                "TXN-" + System.currentTimeMillis() //Added logging
+                "TXN-" + System.currentTimeMillis()
         );
 
         return ResponseEntity.ok(response);
