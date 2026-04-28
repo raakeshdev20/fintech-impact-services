ee53d288d56f7abe829f6d8c8610fcaf3bcdf057
Raakesh Rajagopalan <raakesh20@gmail.com>
2026-04-27 20:17:29 -0400

    Testing impacted file changes payments

--- src/main/java/com/bank/banking_dev_services/payments/PaymentController.java
+++ src/main/java/com/bank/banking_dev_services/payments/PaymentController.java
@@ -23,7 +23,7 @@ public class PaymentController {
         PaymentResponse response = new PaymentResponse(
                 success ? "SUCCESS" : "FAILED",
                 success ? "Payment Successful" : "Payment Failed",
-                "TXN-" + System.currentTimeMillis() // Generates a fake ID for testing
+                "TXN-" + System.currentTimeMillis()
         );
 
         return ResponseEntity.ok(response);
